8fc0f03e40f01e8c507b168a602c80fa2dc4e726
Trien Bang Huynh <trienbanghuynh4701@gmail.com>
2023-01-30 23:00:08 -0800

    update site

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./experience.css";
+import {TfiNewWindow} from 'react-icons/tfi'
 
 const Experience = () => {
-
-
   return (
-    
     <section id="experience">
       <h2> My experiences</h2>
       <div className="container experience_container">
@@ -13,7 +11,16 @@ const Experience = () => {
           <h3>Software Engineer Intern </h3>
           <article className="experience_card">
             <div className="company_date">
-              <div>@Escaype LLC</div>
+              <div >
+                <a 
+                  href="https://www.linkedin.com/company/escaype/"
+                  target="_blank"
+                  rel="noreferrer"
+                 
+                >
+                  @Escaype LLC <TfiNewWindow/>
+                </a>
+              </div>
               <div>Dec. 2022 – Present</div>
             </div>
             <div className="experience_content">
@@ -30,14 +37,23 @@ const Experience = () => {
           <h3>Math-Science Tutor </h3>
           <article className="experience_card">
             <div className="company_date">
-              <div>@De Anza College</div>
+            <div >
+                <a 
+                  href="https://www.deanza.edu/studentsuccess/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @De Anza College <TfiNewWindow/>
+                </a>
+              </div>
               <div>Apr. 2022 – Present</div>
             </div>
             <div className="experience_content">
               <p>
                 {" "}
                 Provide academic support and studying advice to students in
-                lower-division math, physics and chemistry courses through one-on-one in-person, online and individual tutoring sessions.{" "}
+                lower-division math, physics and chemistry courses through
+                one-on-one in-person, online and individual tutoring sessions.{" "}
               </p>
               <p>
                 {" "}
@@ -54,15 +70,23 @@ const Experience = () => {
           <h3>Computer Lab Technician Intern </h3>
           <article className="experience_card">
             <div className="company_date">
-              <div>@De Anza College</div>
-              <div>June 2022 – Present</div>
+            <div >
+                <a 
+                  href="https://www.deanza.edu/oti/comptechs.html"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @De Anza College <TfiNewWindow/>
+                </a>
+              </div>
+              <div>June 2022 – Aug. 2022</div>
             </div>
             <div className="experience_content">
               <p>
                 {" "}
-                Install and test Windows 10 and various application software.
+                Installed and tested Windows 10 and various application software.
               </p>
-              <p> Diagnose and troubleshoot software and hardware issues.</p>
+              <p> Diagnosed and troubleshot software and hardware issues.</p>
             </div>
           </article>
         </div>
@@ -72,21 +96,30 @@ const Experience = () => {
           <h3>Computer Lab Teaching Assistant </h3>
           <article className="experience_card">
             <div className="company_date">
-              <div>@De Anza College</div>
-              <div>Sep. 2021 – Present</div>
+            <div >
+                <a 
+                  href="https://www.deanza.edu/cis/tutoringOnline.html"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @De Anza College <TfiNewWindow/>
+                </a>
+              </div>
+              <div>Sep. 2021 – Dec. 2022</div>
             </div>
             <div className="experience_content">
               <p>
                 {" "}
-                Assist students in fixing logic errors and syntax errors in
+                Assisted students in fixing logic errors and syntax errors in
                 programming assignments.
               </p>
               <p>
-                Provide suggestions on optimizing coding solutions and
+                Provided suggestions on optimizing coding solutions and
                 programming techniques.
               </p>
               <p>
-                Instruct basic programming concepts in C++, Java, Python and problem-solving skills.
+                Instructed basic programming concepts in C++, Java, Python and
+                problem-solving skills.
               </p>
             </div>
           </article>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import {TfiNewWindow} from 'react-icons/tfi'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://www.dropbox.com/s/kti0cankyhlkwmz/my%20resume.pdf?dl=0" target="_blank" rel="noreferrer"  className='btn'>View my resume<TfiNewWindow/></a>
+        <a href="https://www.dropbox.com/s/kti0cankyhlkwmz/my%20resume.pdf?dl=0" target="_blank" rel="noreferrer"  className='btn'>View my resume <TfiNewWindow/></a>
         <a href='#contact' className='btn btn-primary'>Message me</a>
     </div>
   )
